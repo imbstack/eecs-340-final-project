29080fc727d572f5248eb788fbd2de71acddb58e
Brian Stack <bis12@case.edu>
2010-10-23 15:31:02 -0400

    Create a graph visualization

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,36 @@
+import java.awt.*;
+import javax.swing.*;
 import edu.uci.ics.jung.graph.*;
 import edu.uci.ics.jung.graph.util.*;
+import edu.uci.ics.jung.visualization.*;
+import edu.uci.ics.jung.algorithms.layout.*;
 class Main{
 	public static void main(String[] args){
-		// Graph<V, E> where V is the type of the vertices
-		// // and E is the type of the edges
-		Graph<Integer, String> g = new SparseMultigraph<Integer, String>();
-		// // Add some vertices. From above we defined these to be type Integer.
+		SimpleGraphView sgv = new SimpleGraphView(); //We create our graph in here
+		// The Layout<V, E> is parameterized by the vertex and edge types
+		Layout<Integer, String> layout = new CircleLayout(sgv.g);
+		layout.setSize(new Dimension(300,300)); // sets the initial size of the space
+		// The BasicVisualizationServer<V,E> is parameterized by the edge types
+		BasicVisualizationServer<Integer,String> vv =
+		new BasicVisualizationServer<Integer,String>(layout);
+		vv.setPreferredSize(new Dimension(350,350)); //Sets the viewing area size
+		JFrame frame = new JFrame("Simple Graph View");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(vv);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}
+
+class SimpleGraphView{
+	public Graph<Integer, String> g;
+	public SimpleGraphView(){
+		g = new SparseMultigraph<Integer, String>();
 		g.addVertex((Integer)1);
 		g.addVertex((Integer)2);
 		g.addVertex((Integer)3);
-		// // Add some edges. From above we defined these to be of type String
-		// // Note that the default is for undirected edges.
 		g.addEdge("Edge-A", 1, 2); // Note that Java 1.5 auto-boxes primitives
 		g.addEdge("Edge-B", 2, 3);
-		// // Let's see what we have. Note the nice output from the
-		// // SparseMultigraph<V,E> toString() method
-		System.out.println("The graph g = " + g.toString());
-		// // Note that we can use the same nodes and edges in two different graphs.
-		Graph<Integer, String> g2 = new SparseMultigraph<Integer, String>();
-		g2.addVertex((Integer)1);
-		g2.addVertex((Integer)2);
-		g2.addVertex((Integer)3);
-		g2.addEdge("Edge-A", 1,3);
-		g2.addEdge("Edge-B", 2,3, EdgeType.DIRECTED);
-		g2.addEdge("Edge-C", 3, 2, EdgeType.DIRECTED);
-		g2.addEdge("Edge-P", 2,3); // A parallel edge
-		System.out.println("The graph g2 = " + g2.toString());
 	}
 }
