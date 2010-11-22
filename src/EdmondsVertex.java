5364876e015ef14a6ef65e3d960d12bbe768f726
Tom Dooner <tomdooner@gmail.com>
2010-11-21 22:00:36 -0500

    EdmondsKarp should be in more-or-less functional condition now.

--- src/EdmondsVertex.java
+++ src/EdmondsVertex.java
@@ -1,7 +1,12 @@
- 
 public class EdmondsVertex {
     String name;
+    int discoveredState;
+    EdmondsVertex parentNode;
+    long pathCapacityToNode;
+
     EdmondsVertex(String name) {
         this.name = name;
+        this.discoveredState = -1; // undiscovered
+        this.pathCapacityToNode = 4294967296L; // = max int + 1 (roughly equal to infinity)
     }
 }
